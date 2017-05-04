6000f8d7fa9f2ac4f4ac74c2c3754e7cba919c2e
Galen Weld <galen.weld@gmail.com>
2017-05-04 03:25:41 -0400

    rewrote main loop to have less redundant code, all seems well

--- serial_prototype/serial_prototype.ino
+++ serial_prototype/serial_prototype.ino
@@ -24,6 +24,13 @@ Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 // Stuff for thermometer
 TMP102 thermometer(0x48);
 
+// Medication Information
+// These encode the maximum, and minimum, temp limits for each med.
+// Defined in order: Albuterol, Aspirin, Epi, Glucose
+const int med_high_temp[] = {80, 100, 100, 100};
+const int med_low_temp[] = {0, 0, 32, 0};
+
+
 // Definitions to make code more readable
 #define ALB (0)  // albuterol
 #define ASA (2)  // aspirin
@@ -33,6 +40,10 @@ TMP102 thermometer(0x48);
 #define NOM (0)  // medication is normal
 #define EXP (1)  // medication is expired
 #define HOT (2)  // medication has exceeded environmental constraints
+
+#define MED_TYPE   (0) // medication type
+#define MED_EXP    (1) // medication expiration date
+#define MED_STATUS (2) // medication status
 
 // Global Var Defs
 float temp;
@@ -72,20 +83,27 @@ void loop(void) {
   report_status();
 
   // Look for a Card
+  find_and_update_card();
+
+  // Increment the date
+  date++;
+
+}
+
+void find_and_update_card(void) {
+  // Waits until a card is found, reads its info, determines if the med is ok, and then
+  // updates the card and the global medStatus and delays.
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
     
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  // wait until card is scanned for uid and uidLength to populate
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
   
   if (success) {
     Serial.println("Found a card");
     
-    if (uidLength == 4)
-    {
+    if (uidLength == 4) {
       // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
       Serial.println("Trying to authenticate block 4 with default KEYA value");
       uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
@@ -101,71 +119,42 @@ void loop(void) {
     
         if (success)
         {
-          // Dispatch to the appropriate helper
-          Serial.println("Determining Med Type");
-          int med_type = (int) data[0];
-          int expires = (int) data[1];
-          int valid = (int) data[2];
-          
-          if (med_type == 0) albuterol(expires, valid);
-          if (med_type == 1) aspirin(expires, valid);
-          if (med_type == 2) epi(expires, valid);
-          if (med_type == 3) glucose(expires, valid);
-          
-          // bad meds are given type = 5, they are ignored
-          if (med_type > 3) Serial.println("found a bad med");
+          // Call Helper          
+          med_handler((int)data[MED_TYPE], (int)data[MED_EXP], (int)data[MED_STATUS]);
       
           // Wait a bit before reading the card again
           delay(1000);
         }
-        else
-        {
-          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
-        }
+        else {Serial.println("Ooops ... unable to read the requested block.  Try another key?");}
       }
-      else
-      {
-        Serial.println("Ooops ... authentication failed: Try another key?");
-      }
+      else {Serial.println("Ooops ... authentication failed: Try another key?");}
     }
-    
-    if (uidLength != 4)
-    {
-      Serial.println("Card UID of improper length error.");
-
-    }
+    else {Serial.println("Card UID of improper length - error.");}
   }
-  date++; // inc date
 }
 
 void report_status(void) {
   // Reads the status from global variables and updates information on display.
   
   // Med Status
-  if (medStatus[0]==0 && medStatus[1]==0 && medStatus[2]==0 && medStatus[3]==0) {
-    Serial.println("LCD: System Normal");
-  }
-  else if (medStatus[0] != 0) {
-    // bad albuterol
-    if (medStatus[0] == 1) Serial.println("LCD: Albuterol Expired");
-    if (medStatus[0] == 2) Serial.println("LCD: Albuterol out of temp");
+  if (medStatus[ALB] != NOM) {
+    if (medStatus[ALB] == EXP) Serial.println("LCD: Albuterol Expired");
+    if (medStatus[ALB] == HOT) Serial.println("LCD: Albuterol out of temp");
   }
   
-  else if (medStatus[1] != 0) {
-    // bad aspirin
-    if (medStatus[1] == 1) Serial.println("LCD: Aspirin Expired");
-    if (medStatus[1] == 2) Serial.println("LCD: Aspirin out of temp");
+  else if (medStatus[ASA] != NOM) {
+    if (medStatus[ASA] == EXP) Serial.println("LCD: Aspirin Expired");
+    if (medStatus[ASA] == HOT) Serial.println("LCD: Aspirin out of temp");
   }
-  else if (medStatus[2] != 0) {
-    // bad epi
-    if (medStatus[2] == 1) Serial.println("LCD: EpiPen Expired");
-    if (medStatus[2] == 2) Serial.println("LCD: EpiPen out of temp");
+  else if (medStatus[EPI] != NOM) {
+    if (medStatus[EPI] == EXP) Serial.println("LCD: EpiPen Expired");
+    if (medStatus[EPI] == HOT) Serial.println("LCD: EpiPen out of temp");
   }
-  else if (medStatus[3] != 0) {
-    // bad glucose
-    if (medStatus[3] == 1) Serial.println("LCD: Glucose Expired");
-    if (medStatus[3] == 2) Serial.println("LCD: Glucose out of temp");
+  else if (medStatus[GLC] != NOM) {
+    if (medStatus[GLC] == EXP) Serial.println("LCD: Glucose Expired");
+    if (medStatus[GLC] == HOT) Serial.println("LCD: Glucose out of temp");
   }
+  else {Serial.println("LCD: System Normal");}
 
   // Date and Temp
   Serial.print("LCD: Temperature: ");
@@ -174,62 +163,32 @@ void report_status(void) {
   Serial.print("LCD: Date: ");
   Serial.println(date);
   Serial.println("");
-
-  // DEBUGGING
-  Serial.println("meds");
-  Serial.print(medStatus[0]);Serial.print(medStatus[1]);Serial.print(medStatus[2]);Serial.println(medStatus[3]);
-  
 }
 
 
-// Medication handlers
 void med_handler(int m, int e, int v) {
   // universal med handler. takes med, exp, and valid
   // updates and invalidates as needed
-}
+  if (date > e || v == EXP) invalidate_med(m, e, EXP);
+  else if (temp > med_high_temp[m] || v == HOT) invalidate_med(m, e, HOT);
+  else if (temp < med_low_temp[m] || v == HOT) invalidate_med(m, e, HOT);
 
-void albuterol(int e, int v) {
-  Serial.println("Found Albuterol");
-  if (date > e || v == 1) {
-    // med is expired
-    invalidate_med(0, 1);
-    Serial.println("expired");
+  else medStatus[m] = NOM;
   }
-  else if (temp > 80 || v == 2) {
-    // med too hot
-    invalidate_med(0, 2);
-    Serial.println("hot");
-  }
-  else {
-    Serial.println("med ok");
-    medStatus[0] = 0;
-    }
 
-    Serial.print("exp");Serial.println(e);
-    Serial.print("val");Serial.println(v);
-}
-
-void aspirin(int e, int v) {
-  Serial.println("Found Aspirin");
-}
-
-void epi(int e, int v) {
-  Serial.println("Found EpiPen");
-}
-
-void glucose(int e, int v) {
-  Serial.println("Found Glucose");
-}
 
 // Medication Updates
-void invalidate_med(int med, int reason) {
-  // immediately erases the scanned medication and invalidates it
-  // then, updates system status for the given med with the given reason
+void invalidate_med(int med, int e, int reason) {
+  // immediately invalidates the scanned medication by updating its
+  // tag with the given reason while preserving other info
+  // then, updates system status as well
   
   uint8_t data[16];
   uint8_t success;
   uint8_t bad_med[16] = { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-  bad_med[0] = med; bad_med[2] = reason;
+  bad_med[MED_TYPE] = med;
+  bad_med[MED_STATUS] = reason;
+  bad_med[MED_EXP] = e;
   memcpy(data, bad_med, sizeof data);
   success = nfc.mifareclassic_WriteDataBlock (4, data);
 
