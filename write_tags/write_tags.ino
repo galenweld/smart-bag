03fa2b2aa7379893583bf931c797ccf7f72d635a
Galen Weld <galen.weld@gmail.com>
2017-05-04 00:48:36 -0400

    added function to write data to tags

--- write_tags/write_tags.ino
+++ write_tags/write_tags.ino
@@ -2,54 +2,44 @@
 
 /**************************************************************************/
 /*! 
-    @file     readMifare.pde
-    @author   Adafruit Industries
-  @license  BSD (see license.txt)
+Writes NFC information to represent a medication to a card.
+Data is written to the first block of sector 1.
+The relevant information is stored in the first three spots of the block,
+in the following order:
 
-    This example will wait for any ISO14443A card or tag, and
-    depending on the size of the UID will attempt to read from it.
-   
-    If the card has a 4-byte UID it is probably a Mifare
-    Classic card, and the following steps are taken:
-   
-    - Authenticate block 4 (the first block of Sector 1) using
-      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
-    - If authentication succeeds, we can then read any of the
-      4 blocks in that sector (though only block 4 is read here)
-   
-    If the card has a 7-byte UID it is probably a Mifare
-    Ultralight card, and the 4 byte pages can be read directly.
-    Page 4 is read by default since this is the first 'general-
-    purpose' page on the tags.
+MEDICATION
+0 - Albuterol
+1 - Aspirin
+2 - Epinepherine
+3 - Glucose
 
+EXPIRATION
+A single value 0..99 representing the expiration date.
 
-This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-This library works with the Adafruit NFC breakout 
-  ----> https://www.adafruit.com/products/364
- 
-Check out the links above for our tutorials and wiring diagrams 
-These chips use SPI or I2C to communicate.
+VALID
+0 for valid
+1 for invalid
 
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
+For example 02:15:00 would represent a EpiPen expiring at 15 that is valid.
 
 */
 /**************************************************************************/
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
-
-
 #define PN532_SCK  (2)
 #define PN532_MOSI (3)
 #define PN532_SS   (4)
 #define PN532_MISO (5)
-
-// If using the breakout or shield with I2C, define just the pins connected
-// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
 #define PN532_IRQ   (2)
 #define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+/**************************************************************************/
+/* Enter Data to Write Below:
+/**************************************************************************/
+const uint8_t med[] = { 2, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
 
 // Use this line for a breakout with a software SPI connection (recommended):
 Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
@@ -117,7 +107,7 @@ void loop(void) {
     
         // If you want to write something to block 4 to test with, uncomment
         // the following line and this text should be read back in a minute
-        memcpy(data, (const uint8_t[]){ 1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, sizeof data);
+        memcpy(data, med, sizeof data);
         success = nfc.mifareclassic_WriteDataBlock (4, data);
 
         // Try to read the contents of block 4
